9d3e127bf6df9459fdfe1253aa9ee44bd8c5fab1
veakev <vekaev4@gmail.com>
2020-12-25 16:27:00 +0200

    comment test code

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,9 +12,9 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      if (component.name) {
-        window['c' + component.name] = component
-      }
+      // if (component.name) {
+      //   window['c' + component.name] = component
+      // }
       $el.html(component.toHTML())
       $root.append($el)
       return component
