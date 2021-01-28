a14ed581fb38a38290bc5020515da48b797a385d
ypetrenko <vekaev4@gmail.com>
2021-01-28 11:04:45 +0200

    create unit testing

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,5 +1,5 @@
 import {Excel, Header, Toolbar, Formula, Table} from '@/components'
-import {createStore} from '@core/createStore';
+import {createStore} from '@core/store/createStore';
 import {rootReducer} from '@/redux/rootReducer';
 import {storage, debounce} from '@core/utils';
 import {normalizeInitialState} from '@/redux/initialState';
